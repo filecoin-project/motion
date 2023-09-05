3866c4a697d2c3beb4bc76ba8fe75094da0b5c3e
hannahhoward <hannah@hannahhoward.net>
2023-09-05 10:10:24 -0700

    fix(cli): allow motion sp providers set from environment

--- cmd/motion/main.go
+++ cmd/motion/main.go
@@ -74,6 +74,7 @@ func main() {
 				Aliases:     []string{"sp"},
 				Usage:       "Storage providers to which to make deals with. Multiple providers may be specified.",
 				DefaultText: "No deals are made to replicate data onto storage providers.",
+				EnvVars:     []string{"MOTION_STORAGE_PROVIDERS"},
 			},
 			&cli.StringFlag{
 				Name:     "lotusApi",
