455c589bf3a16000607f48c9d3b0780f4a889d16
hannahhoward <hannah@hannahhoward.net>
2023-09-22 23:47:52 -0700

    fix(test): buildsingularity.sh use ref

--- integration/test/buildsingularity.sh
+++ integration/test/buildsingularity.sh
@@ -4,5 +4,5 @@ if [ -n "$SINGULARITY_LOCAL_DOCKERFILE" ]
 then
   echo "Building singularity form local source"
   source ./motionlarity/.env
-  docker build -t ghcr.io/data-preservation-programs/singularity:${SINGULARITY_TAG} ${SINGULARITY_LOCAL_DOCKERFILE}
+  docker build -t ghcr.io/data-preservation-programs/singularity${SINGULARITY_REF} ${SINGULARITY_LOCAL_DOCKERFILE}
 fi
