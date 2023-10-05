b894341587780aef69aeabe4bd4501f8fa0288c0
Masih H. Derkani <m@derkani.org>
2023-10-05 08:58:14 +0100

    Update integration/test/buildsingularity.sh
    Co-authored-by: Elijah <elijaharita@gmail.com>

--- integration/test/buildsingularity.sh
+++ integration/test/buildsingularity.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$SINGULARITY_LOCAL_DOCKERFILE" ]
 then
-  echo "Building singularity form local source"
+  echo "Building singularity from local source"
   source ./motionlarity/.env
   docker build -t ghcr.io/data-preservation-programs/singularity${SINGULARITY_REF} ${SINGULARITY_LOCAL_DOCKERFILE}
 fi
